1feb058cd5fcf76eb073d04e34aff3f0c1a1e59f
Samer Mobarak <samermobarak888@gmail.com>
2023-09-12 14:45:52 -0400

    simple_liker

--- main.js
+++ main.js
@@ -2,7 +2,69 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+  const errorModal = document.querySelector("#modal");
+  errorModal.classList.add("hidden");
+
+  const hearts = document.querySelectorAll(".like-glyph");
+
+  hearts.forEach((heart) => {
+    heart.addEventListener("click", () => {
+      mimicServerCall()
+        .then(() => {
+          if (!heart.classList.contains("activated-heart")) {
+            heart.classList.add("activated-heart");
+            heart.innerHTML = fullHeart;
+          } else {
+            heart.classList.remove("activated-heart");
+            heart.innerHTML = emptyHeart;
+          }
+        })
+        .catch(() => {
+          errorModal.classList.remove("hidden");
+          const errorMessage = document.querySelector("#modal-message");
+          errorMessage.textContent = "Server Error: Request Failed";
+
+          setTimeout(() => {
+            errorModal.classList.add("hidden");
+          }, 3000);
+        });
+    });
+  });
+});
+
+// Simulated server call function
+function mimicServerCall() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const random = Math.random();
+      if (random < 0.2) {
+        reject("Server Error: Request Failed");
+      } else {
+        resolve("Success");
+      }
+    }, 1000);
+  });
+}
+
+const fullHeart = `<span class="like-glyph activated-heart">&#x2764;</span>`;
+const emptyHeart = `<span class="like-glyph">&#x2661;</span>`;
+
+
+// Simulated server call function
+function mimicServerCall() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const random = Math.random();
+      if (random < 0.2) {
+        reject("Server Error: Request Failed");
+      } else {
+        resolve("Success");
+      }
+    }, 1000);
+  });
+}
+
 
 
 
